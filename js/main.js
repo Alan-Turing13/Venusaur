787d3f9ebe1f9edb68c8f3607990acbec7bf505c
dMarsh <125702046+Alan-Turing13@users.noreply.github.com>
2023-12-10 19:56:39 +0000

    deleted unnecessary files

--- js/main.js
+++
@@ -1,7 +0,0 @@
-function updateDateTime() {
-  const now = new Date();
-  const options = { day: 'numeric', month: 'short', year: 'numeric' };
-  const currentDateTime = now.toLocaleString('en-US', options);
-  document.querySelector('#date').textContent = currentDateTime;
-}
-setInterval(updateDateTime, 1000);
